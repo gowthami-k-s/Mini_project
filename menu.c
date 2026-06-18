4929e349d7c5dcf1ba038ccd226b0e2c8b9be804
gowthami-k-s <gowthamiks2004@gmail.com>
2026-06-18 06:37:32 +0000

    first commit

---
+++ menu.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+    char op;
+    printf("<---------WElOCOME TO HOTEL--------->\n");
+    printf("Select what you want to eat \n");
+    printf("1. Starter\n2. Main course\n3. Dessert\n4. Exit\n");
+    scanf("%c",&op);
+    switch(op)
+    {
+        case '1':
+        printf("You have chosen starter\n");
+        printf("1.Gobi 65\n2.Chicken 65\n3.");
+        switch(op)
+        {
+
+        }
+        break;
+        case '2':
+        printf("You have chosen main course\n");
+        printf("1. \n2. \n3. ");
+        switch(op)
+        {
+
+            
+        }
+        break;
+        case '3':
+        printf("You have chosen desserts\n");
+        printf("1. \n2. \n3. ");
+        switch(op)
+        {
+
+        }
+        break;
+        case '4':
+        printf("<--THANK YOU VISIT AGAIN-->\n");
+        break;
+        case
+    }
+}
