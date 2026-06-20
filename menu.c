3ca1911e1271d39beae395a7340bff46d66d5c93
gowthami-k-s <gowthamiks2004@gmail.com>
2026-06-20 10:27:50 +0000

    third commit

--- menu.c
+++ menu.c
@@ -2,84 +2,110 @@
 int main()
 {
     char op;
-    int item,quant;
+    int i=1;
+    int item,quant,price=0;
+    float total=0;
     printf("<---------WElOCOME TO HOTEL--------->\n");
     printf("Select what you want to eat \n");
-    printf("1. Starter\n2. Main course\n3. Dessert\n4. Exit\n");
-    scanf("%c",&op);
-    switch(op)
+    do
+    {
+        printf("1. Starter\n2. Main course\n3. Dessert\n4. Exit\n");
+        scanf(" %c",&op);
+        switch(op)
     {
         case '1':
         printf("You have chosen starter\n");
-        printf("1.Gobi 65 = 100\n2.Chicken 65 = 120\n3.Chilli chicken = 150");
+        printf("1.Gobi 65 = 100\n2.Chicken 65 = 120\n3.Chilli chicken = 150\n");
         printf("Item: ");
         scanf("%d",&item);
         printf("Quantity: ");
         scanf("%d",&quant);
         if(item==1)
         {
-            int price=100*quant;
+            price=100*quant;
             printf("Price: %d\n",price);
         }
         else if(item==2)
         {
-            int price=120*quant;
+            price=120*quant;
             printf("Price: %d\n",price);
         }
-        if(item==3)
+        else if(item==3)
         {
-            int price=150*quant;
+            price=150*quant;
             printf("Price: %d\n",price);
+        }
+        else
+        {
+            printf("Noo item mentioned");
         }
         break;
         case '2':
         printf("You have chosen main course\n");
-        printf("1.Chicken biryani = 200 \n2.Naan and Butter chicken = 180\n3.Thali = 320");
+        printf("1.Chicken biryani = 200 \n2.Naan and Butter chicken = 180\n3.Thali = 320\n");
         printf("Item: ");
         scanf("%d",&item);
         printf("Quantity: ");
         scanf("%d",&quant);
         if(item==1)
         {
-            int price=200*quant;
+            price=200*quant;
             printf("Price: %d\n",price);
         }
         else if(item==2)
         {
-            int price=180*quant;
+            price=180*quant;
             printf("Price: %d\n",price);
         }
-        if(item==3)
+        else if(item==3)
         {
-            int price=320*quant;
+            price=320*quant;
             printf("Price: %d\n",price);
+        }
+        else
+        {
+            printf("Noo item mentioned");
         }
         break;
         case '3':
         printf("You have chosen desserts\n");
-        printf("1.Ice cream = 50 \n2.Gulaab jamun = 60 \n3.Falooda = 100");
-        printf("\nItem: ");
+        printf("1.Ice cream = 50 \n2.Gulaab jamun = 60 \n3.Falooda = 100\n");
+        printf("Item: ");
         scanf("%d",&item);
         printf("Quantity: ");
         scanf("%d",&quant);
         if(item==1)
         {
-            int price=50*quant;
+            price=50*quant;
             printf("Price: %d\n",price);
         }
         else if(item==2)
         {
-            int price=60*quant;
+            price=60*quant;
             printf("Price: %d\n",price);
         }
-        if(item==3)
+        else if(item==3)
         {
-            int price=100*quant;
+            price=100*quant;
             printf("Price: %d\n",price);
+        }
+        else
+        {
+            printf("Noo item mentioned");
         }
         break;
-        default:
+        case '4':
         printf("<--THANK YOU VISIT AGAIN-->\n");
         break;
+        default:
+        printf("No items listed");
+        break;
     }
+    total+=price;
+    i++;
+}while(i != 4);
+    printf("==========================\n");
+    printf("Total bill with gst = %g\n",(float)total-(total*0.18));
+    printf("Thank you for visiting\n");
+    printf("==========================\n");
 }
