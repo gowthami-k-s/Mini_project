d0b23025f572a77130826e93832c20749148b91b
gowthami-k-s <gowthamiks2004@gmail.com>
2026-06-21 12:48:21 +0000

    fifth commit

--- menu.c
+++ menu.c
@@ -2,9 +2,10 @@
 int main()
 {
     char op;
-    int i=1;
-    int item,quant,price=0;
-    float total=0;
+    int item,quant;
+    int price=0;
+    int g=0,c=0,cc=0,cb=0,nb=0,t=0,ic=0,gj=0,f=0;
+    float total=0,gst=0;
     printf("<---------WElOCOME TO HOTEL--------->\n");
     printf("Select what you want to eat \n");
     do
@@ -12,7 +13,7 @@ int main()
         printf("1. Starter\n2. Main course\n3. Dessert\n4. Exit\n");
         scanf(" %c",&op);
         switch(op)
-    {
+        {
         case '1':
         printf("You have chosen starter\n");
         printf("1.Gobi 65 = 100\n2.Chicken 65 = 120\n3.Chilli chicken = 150\n");
@@ -23,17 +24,17 @@ int main()
         if(item==1)
         {
             price=100*quant;
-            printf("Price: %d\n",price);
+            g+=quant;
         }
         else if(item==2)
         {
             price=120*quant;
-            printf("Price: %d\n",price);
+            c+=quant;
         }
         else if(item==3)
         {
             price=150*quant;
-            printf("Price: %d\n",price);
+            cc+=quant;
         }
         else
         {
@@ -50,17 +51,17 @@ int main()
         if(item==1)
         {
             price=200*quant;
-            printf("Price: %d\n",price);
+            cb+=quant;
         }
         else if(item==2)
         {
             price=180*quant;
-            printf("Price: %d\n",price);
+            nb+=quant;
         }
         else if(item==3)
         {
             price=320*quant;
-            printf("Price: %d\n",price);
+            t+=quant;
         }
         else
         {
@@ -77,17 +78,17 @@ int main()
         if(item==1)
         {
             price=50*quant;
-            printf("Price: %d\n",price);
+            ic+=quant;
         }
         else if(item==2)
         {
             price=60*quant;
-            printf("Price: %d\n",price);
+            gj+=quant;
         }
         else if(item==3)
         {
             price=100*quant;
-            printf("Price: %d\n",price);
+            f+=quant;
         }
         else
         {
@@ -95,16 +96,56 @@ int main()
         }
         break;
         case '4':
-        printf("<--THANK YOU VISIT AGAIN-->\n");
+        printf("\n");
         break;
         default:
         printf("No items listed");
         break;
+        }
+        total+=price;
+    }while(op != '4');
+    printf("Bill summary\n");
+    if(g>0)
+    {
+        printf("Gobi 65 X %d = %d\n",g,g*100);
     }
-    total+=price;
-    i++;
-}while(i != 4);
+    if(c>0)
+    {
+        printf("Chicken 65 X %d = %d\n",c,c*120);
+    }
+    if(cc>0)
+    {
+        printf("Chilli chicken X %d = %d\n",cc,cc*150);
+    }
+    if(cb=0)
+    {
+        printf("Chicken biryani  X %d = %d\n",cb,cb*200);
+    }
+    if(nb>0)
+    {
+        printf("Naan and Butter chicken X %d = %d\n",nb,nb*180);
+    }
+    if(t>0)
+    {
+        printf("Thali X %d = %d\n",t,t*320);
+    }
+    if(ic>0)
+    {
+        printf("Ice cream   X %d = %d\n",ic,ic*50);
+    }
+    if(gj>0)
+    {
+        printf("Gulaab jamun X %d = %d\n",gj,gj*60);
+    }
+    if(f>0)
+    {
+        printf("Falooda X %d = %d\n",f,f*100);
+    }
     printf("==========================\n");
-    printf("Total bill with gst = %g\n",(float)total-(total*0.18));
+    printf("<--THANK YOU VISIT AGAIN-->\n");
+    printf("Sub total = %g\n",total);
+    gst=total*0.18;
+    printf("Gst = %g\n",gst);
+    printf("Total bill with gst = %g\n",total+gst);
     printf("==========================\n");
 }
