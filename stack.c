bcf575f690eb4deb99fba33456df08834b83591d
gowthami-k-s <gowthamiks2004@gmail.com>
2026-07-01 06:10:09 +0000

    first commit

---
+++ stack.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+int main()
+{
+    //Implementation of stack using array
+    int arr[5];
+    int op,size = 0;
+    while(1)
+    {
+        printf("1. Insertion\n2. Deletion\n3. Printing the Stack\n4. Exit\nEnter Your choice : ");
+        scanf("%d",&op);
+        switch(op)
+        {
+            case 1:
+            //Insertion of elements into Stack
+            if(size >= 5)
+            {
+                printf("\"Stack is full\"\n");
+                printf("\n");
+                break;
+            }
+            int data;
+            printf("Enter the element to insert: ");
+            scanf("%d",&data);
+            arr[size++] = data;
+            printf("\n");
+            break;
+            case 2:
+            //Deletion of elements from Stack
+            if(size <= 0)
+            {
+                printf("\"Stack is empty\"\n");
+                printf("\n");
+                break;
+            }
+            printf("\"Deleted element: %d\"3\n", arr[size - 1]);
+            printf("\n");
+            size--;
+            break;
+            case 3:
+            //Print Stack elements
+            if(size == 0)
+            {
+                printf("\"Stack is empty\"\n");
+                printf("\n");
+                break;
+            }
+            printf("Elements of Stack : ");
+            for(int i=0 ; i<size ; i++)
+            {
+                printf("%d ",arr[i]);
+            }
+            printf("\n\n");
+            break;
+            case 4:
+            //Exit stack
+            printf("\"Exiting.......\"\n");
+            return 0;
+            default:
+            printf("\"Invalid input\"\n");
+            printf("\n");
+            break;
+        }
+    }
+} 
